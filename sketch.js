0826d4229a29ea35a7707f2180053f84da89af2f
grevy0810 <grrinconm@unal.edu.co>
2025-02-16 22:27:40 -0500

    feat: add labels and fear slider

--- sketch.js
+++ sketch.js
@@ -5,8 +5,12 @@ let rot =  0;
 let canvas, img, offset;
 let shaderFog;
 let sliderNear;
-let fogDensity = 0.002;
+let sliderFar;
+let fogDensity, fogFarValue;
 let fogColor = [0.8, 0.9, 1, 1];
+let font;
+let farInit =0.5;
+let nearInit =0.25;
 function preload() {
   img = loadImage('https://webglfundamentals.org/webgl/resources/f-texture.png');
 }
@@ -17,11 +21,27 @@ function setup() {
   frameRate(60);
   angleMode(DEGREES);
   shaderFog = createShader(shaderFogVert, shaderFogFrag);
-  
-  sliderNear = createSlider(0, 1.0, 0.25, 0.05);
+
+  sliderNear = createSlider(0, 1.0, nearInit , 0.05);
   sliderNear.position(windowWidth/2 - 100, 130);
   sliderNear.size(200);
 
+  let pNear = createP('Near:');
+  pNear.position(sliderNear.x- 40, sliderNear.y -17);
+  let pNearValue = createP(`${nearInit}`);
+  pNearValue.position(sliderNear.x + 220, sliderNear.y -17);
+  sliderNear.input(() => updateFogValue(pNearValue, sliderNear));
+
+
+  sliderFar = createSlider(0, 1.0, farInit, 0.05); // Nuevo slider para el fogFar
+  sliderFar.position(windowWidth / 2 - 100, 160);
+  sliderFar.size(200);
+
+  let pFar = createP('Far:');
+  pFar.position(sliderFar.x- 40, sliderFar.y -17);
+  let pFarValue = createP(`${farInit}`);
+  pFarValue.position(sliderFar.x + 220, sliderFar.y -17);
+  sliderFar.input(() => updateFogValue(pFarValue, sliderFar));
   noStroke();
 }
 
@@ -34,9 +54,10 @@ function draw() {
 
   rot += 0.3;
   fogDensity = sliderNear.value();
+  fogFarValue = sliderFar.value();
 
-  shaderFog.setUniform("fillCol", [0.2,0.2,0.5]);
   shaderFog.setUniform("uFogColor", fogColor);
+  shaderFog.setUniform("uFogFar", fogFarValue);
   shaderFog.setUniform("uFogNear", fogDensity);
   
   shader(shaderFog);
@@ -53,25 +74,33 @@ function draw() {
     pop();
   }
   pop();
-
+  
 }
-
-function windowResized(){
-  resizeCanvas(windowWidth,windowHeight);
-  sliderNear.position(windowWidth/2 - 100, 130);
+function updateFogValue(label, slider) {
+  label.html(`${slider.value()}`);
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  sliderNear.position(windowWidth / 2 - 100, 130);
+  sliderFar.position(windowWidth / 2 - 100, 160);
 }
 
 
 function isMouseOverSlider() {
   let x = windowWidth / 2 - 100;
-  let y = 130;
   let w = 200;
   let h = 20;
 
-  if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h && mouseIsPressed) {
-    return true;
+  let yNear = 130;
+  let yFar = 160; 
+
+  if (
+    (mouseX > x && mouseX < x + w && mouseY > yNear && mouseY < yNear + h) ||
+    (mouseX > x && mouseX < x + w && mouseY > yFar && mouseY < yFar + h)
+  ) {
+    return mouseIsPressed;
   }
-  return false;
+  return false
 }
 
 
@@ -112,25 +141,15 @@ uniform sampler2D sTexture;
  
 uniform vec4 uFogColor;
 uniform float uFogNear;
- 
-uniform vec3 fillCol;
+uniform float uFogFar;
 
 void main() {
-  // dibuja la textura y el color de relleno
-  vec3 filler;
-  filler= fillCol ;
   
-  vec4 col = texture2D(sTexture, vTexCoord).rgba;
-  if (col.a < 1.0){
-    col = vec4(filler,1.0);
-  }
+   vec4 col = texture2D(sTexture, vTexCoord).rgba;
 
-  #define LOG2 1.442695
-  // Se añade el fog
-  float fogDist = length(vPos)/100.0;
-  float fogAmount = 1. - exp2(-uFogNear * uFogNear * fogDist * fogDist * LOG2);
-  fogAmount = clamp(fogAmount, 0., 1.);
-  gl_FragColor = mix(col, uFogColor, fogAmount);  
+  float fogAmount = smoothstep(uFogNear, uFogFar, gl_FragCoord.z);
+  // Mezcla entre el color original y el color de la niebla.
+  gl_FragColor = mix(col, uFogColor, fogAmount);
 }
 
 `;

c7c6d725066c4459c558a97eed013e2e83823a8f
grevy0810 <grrinconm@unal.edu.co>
2025-02-16 19:11:09 -0500

    add fogNear

--- sketch.js
+++ sketch.js
@@ -1,63 +1,136 @@
-let shaderProgram;
-let img;
-let angleX = 0;
-let angleY = 0;
-let fogColor = [0.8, 0.9, 1, 1]; // Color de la niebla
-let fogNear = 0.7;
-let fogFar = 0.9;
-let xOff = 1.1;
-let zOff = 1.4;
-let numCubes = 40;
 
-// Sliders
-let fogNearSlider, fogFarSlider;
-
+let cam;
+let timeInterval = 60;
+let rot =  0;
+let canvas, img, offset;
+let shaderFog;
+let sliderNear;
+let fogDensity = 0.002;
+let fogColor = [0.8, 0.9, 1, 1];
 function preload() {
-  img = loadImage("https://webglfundamentals.org/webgl/resources/f-texture.png");
-  shaderProgram = loadShader('fog.vert', 'fog.frag');
+  img = loadImage('https://webglfundamentals.org/webgl/resources/f-texture.png');
 }
 
 function setup() {
-  createCanvas(600, 400, WEBGL);
+  createCanvas(windowWidth,  windowHeight, WEBGL);
 
-  // Crear sliders para controlar la niebla
-  createP("Fog Near").style("color", "white");
-  fogNearSlider = createSlider(0, 1, fogNear, 0.01);
+  frameRate(60);
+  angleMode(DEGREES);
+  shaderFog = createShader(shaderFogVert, shaderFogFrag);
   
-  createP("Fog Far").style("color", "white");
-  fogFarSlider = createSlider(0, 1, fogFar, 0.01);
+  sliderNear = createSlider(0, 1.0, 0.25, 0.05);
+  sliderNear.position(windowWidth/2 - 100, 130);
+  sliderNear.size(200);
+
+  noStroke();
 }
 
 function draw() {
-  background(fogColor[0] * 255, fogColor[1] * 255, fogColor[2] * 255);
+  background(fogColor[0]*255,fogColor[1]*255,fogColor[2]*255);
 
-  // Actualizar valores de la niebla desde los sliders
-  fogNear = fogNearSlider.value();
-  fogFar = fogFarSlider.value();
+  if (!isMouseOverSlider()) {
+    orbitControl(0.5, 0, 0);
+  }
 
-  // Configurar cámara
-  perspective(PI / 3, width / height, 0.1, 100);
-  camera(0, 0, 2, 0, 0, 0, 0, 1, 0);
+  rot += 0.3;
+  fogDensity = sliderNear.value();
 
-  // Activar el shader
-  shader(shaderProgram);
-
-  // Pasar parámetros al shader
-  shaderProgram.setUniform("sTexture", img);
-  shaderProgram.setUniform("uFogColor", fogColor);
-  shaderProgram.setUniform("uFogDensity", fogNear);
-  shaderProgram.setUniform("u_fogFar", fogFar);
-
-  angleX += radians(0.4);
-  angleY += radians(0.7);
-
-  for (let i = 0; i <= numCubes; i++) {
+  shaderFog.setUniform("fillCol", [0.2,0.2,0.5]);
+  shaderFog.setUniform("uFogColor", fogColor);
+  shaderFog.setUniform("uFogNear", fogDensity);
+  
+  shader(shaderFog);
+  translate(-100,0,700);
+  push();
+  for (let i = 0; i < 20; i++){
+    translate(16, 0, -64);
     push();
-    translate(-2 + i * xOff, 0, -i * zOff);
-    rotateX(angleX + i * 0.1);
-    rotateY(angleY + i * 0.1);
-    texture(img);
-    box(1);
+    rotateZ(rot);
+    rotateX(rot);
+    rotateY(rot);
+    shaderFog.setUniform("sTexture", img);
+    box(32);
     pop();
   }
+  pop();
+
 }
+
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight);
+  sliderNear.position(windowWidth/2 - 100, 130);
+}
+
+
+function isMouseOverSlider() {
+  let x = windowWidth / 2 - 100;
+  let y = 130;
+  let w = 200;
+  let h = 20;
+
+  if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h && mouseIsPressed) {
+    return true;
+  }
+  return false;
+}
+
+
+const shaderFogVert = `
+precision mediump float;
+
+attribute vec3 aPosition;
+attribute vec2 aTexCoord;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+varying vec2 vTexCoord;
+
+varying vec3 vPos;
+
+void main() {
+  // Camera looking objects
+  vTexCoord = aTexCoord;
+  gl_Position = uProjectionMatrix * 
+    uModelViewMatrix *
+    vec4(aPosition, 1.0); 
+
+  // Position in camera perspective
+  vPos = (uModelViewMatrix * vec4(aPosition, 1.0)).xyz;
+}
+
+`;
+
+const shaderFogFrag = `
+precision mediump float;
+ 
+// Passed in from the vertex shader.
+varying vec2 vTexCoord;
+varying vec3 vPos;
+// The texture.
+uniform sampler2D sTexture;
+ 
+uniform vec4 uFogColor;
+uniform float uFogNear;
+ 
+uniform vec3 fillCol;
+
+void main() {
+  // dibuja la textura y el color de relleno
+  vec3 filler;
+  filler= fillCol ;
+  
+  vec4 col = texture2D(sTexture, vTexCoord).rgba;
+  if (col.a < 1.0){
+    col = vec4(filler,1.0);
+  }
+
+  #define LOG2 1.442695
+  // Se añade el fog
+  float fogDist = length(vPos)/100.0;
+  float fogAmount = 1. - exp2(-uFogNear * uFogNear * fogDist * fogDist * LOG2);
+  fogAmount = clamp(fogAmount, 0., 1.);
+  gl_FragColor = mix(col, uFogColor, fogAmount);  
+}
+
+`;
